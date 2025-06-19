38b563d4df793c691b4cde0d0a6ad201d6a41050
Cleidson Danilo <cleidsondanilo18@gmail.com>
2025-06-19 01:09:18 -0300

    adiçao da linguagem awp para o site ter a funcionalidade de aplicatico em mobiles

---
+++ service-worker.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = "estudafacil-v1";
+const urlsToCache = [
+  "/index.html",
+  "/page/material.html",
+  "/page/cronograma.html",
+  "/page/checklist.html",
+  "/page/relatorio.html",
+  "/page/dicas.html",
+  "/page/materiais.html",
+  "/page/contato.html",
+  "/page/sobre.html",
+  "/style/estilo.css",
+  "/image/studying.ico",
+  "/image/studying.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.filter(name => name !== CACHE_NAME).map(name => caches.delete(name))
+      );
+    })
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
